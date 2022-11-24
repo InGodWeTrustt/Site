797bf3c422187d35d95d9d653e2dcf7c6644ff91
InGodWeTrustt <fanat2016@mail.ru>
2022-11-24 23:05:16 +0300

    еще пару изменений

--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 
-let rerenderEntireTree = () => {
+let rerenderEntireTree = (state) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
-    <React.StrictMode>
-      <App  state={state}/>
-    </React.StrictMode>
+    <>
+      <App
+        state={state}
+        addPost={store.addPost.bind(store)}
+        updateNewPost={store.updateNewPost.bind(store)} />
+    </>
   );
 }
 
-subscribe(rerenderEntireTree)
+rerenderEntireTree(store.getState())
+
+store.subscribe(rerenderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
